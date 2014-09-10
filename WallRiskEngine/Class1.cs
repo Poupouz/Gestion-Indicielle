ce93c3b5eb63e05668c799a2e8240cf01b05f9f0
Romain Pouzaud <romain.pouzaud@orange.fr>
2014-09-10 11:09:52 +0200

    Determine path using system (32 or 64)

--- WallRiskEngine/Class1.cs
+++ WallRiskEngine/Class1.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace WallRiskEngine
 {
+
     public class Class1
     {
+        const string pathToDll = getDllPathFollowingSystem();
+
+        private static String getDllPathFollowingSystem()
+        {
+            if (System.Environment.Is64BitOperatingSystem)
+            {
+                return @"lib\x64\wre-ensimag-c-4.1.dll";
+            } else
+	        {
+                return @"lib\x86\wre-ensimag-c-4.1.dll";
+	        }
+        }
+
     }
 }
