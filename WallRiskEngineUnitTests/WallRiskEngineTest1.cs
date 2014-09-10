21cac5cce3af8c59a8b03e5211ead316c7691e3a
Romain Pouzaud <romain.pouzaud@orange.fr>
2014-09-10 16:24:04 +0200

    Correct name error in unit test

--- WallRiskEngineUnitTests/WallRiskEngineTest1.cs
+++ WallRiskEngineUnitTests/WallRiskEngineTest1.cs
@@ -15,7 +15,7 @@ namespace WallRiskEngineUnitTests
             double[,] netReturns = WallRiskEngine.API.computeSimpleNetReturns(returns, 1);
         }
 
-        public void SimpleNetReturnsTest()
+        public void computeCovarianceMatrixTest()
         {
             double[,] returns = { {0.05, 0.1, 0.6}, {0.001, 0.4, 0.56}, {0.7, 0.001, 0.12}, {0.3, 0.2, 0.1},
                             {0.1, 0.2, 0.3}};
