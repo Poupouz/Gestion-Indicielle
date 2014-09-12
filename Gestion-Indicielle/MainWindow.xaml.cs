4e888d34a588896396bf4908f30007f114ffa62d
Francois Baton <Francois.Baton@ensimag.grenoble-inp.fr>
2014-09-12 11:53:46 +0200

    recuperation donnees portefeuille initial

--- Gestion-Indicielle/MainWindow.xaml.cs
+++ Gestion-Indicielle/MainWindow.xaml.cs
@@ -31,70 +31,27 @@ namespace Gestion_Indicielle
 
         private double[] benchmarkIndex;
         private int numberDays;
+        PortfolioViewModel p;
+        ArrayList tickers;
 
         public MainWindow()
         {
             InitializeComponent();
-            numberDays = 100;
+            numberDays = 1998;
             DataRetriever dr = new DataRetriever();
-            benchmarkIndex = dr.extractColumnIndex(dr.getDataBenchmark(new DateTime(2012, 2, 3, 0, 0, 0), numberDays), 0);
-            this.DataContext = new PortfolioViewModel();
+            benchmarkIndex = dr.extractColumnIndex(dr.getDataBenchmark(new DateTime(2006, 1, 2, 0, 0, 0), numberDays), 0);
+            p = new PortfolioViewModel();
+            this.DataContext = p;
+            
             ViewCharts Chart = new ViewCharts();
             Chart.createSerie(benchmarkIndex,"Cac40");
             lineChart.Series.Add(Chart.series.ElementAt(0));
 
         }
 
-        private List<Object> LoadCompanies()
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            List<Object> result=new List<Object>();
-            DataRetriever dr = new DataRetriever();
-            ArrayList al = dr.getTickers();
-
-
-            /* ------ A ENLEVER POUR PLUS TARD ------ */
-            Console.WriteLine(dr.nbDate());
-            AverageHistoricYield ahy = new AverageHistoricYield();
-
-
-            double[,] matrice = ahy.getMatrixOfPrice(al, new DateTime(2006, 1, 2, 0, 0, 0), 1000);
-
-            double[,] returnsMatrix = ahy.getReturnsMatrix(matrice,1);
-
-            double[] meanReturns = ahy.getMeanReturn(returnsMatrix);
-
-            double[,] covMatrix = ahy.getCovMatrix(returnsMatrix);
-
-            double[,] bench = dr.getDataBenchmark(new DateTime(2006, 1, 2, 0, 0, 0), 1000);
-            double[,] returnsBench = ahy.getReturnsMatrix(bench, 1);
-
-            double[,] concatMat = ahy.concatMatrix(matrice,bench);
-            double[,] covConcat = ahy.getCovMatrix(concatMat);
-
-            double[,] covMatrixExtract = ahy.extractCovReturnAssets(covConcat,covConcat.GetLength(0)-1 );
-            double[]  covVectorExtract = ahy.extractCovReturnBench(covConcat,covConcat.GetLength(0) -1 );
-            double varExtract = ahy.extractVarianceBench(covConcat,covConcat.GetLength(0) -1 );
-            double[] coeff = API.OptimPortfolioWeight(covMatrixExtract, meanReturns, covVectorExtract, ahy.getMeanReturn(returnsBench)[0], 0.000000001);
-            Console.WriteLine(coeff.GetLength(0));
-            double somme = 0.0;
-            for (int i = 0; i < coeff.GetLength(0); i++)
-            {
-                Console.WriteLine(coeff[i]);
-                somme += coeff[i];
-            }
-
-            Console.WriteLine(somme);
-
-                /*-------------------------------------------------------*/
-
-
-                foreach (var v in al)
-                {
-                    result.Add(new { Name = v, IsInPortfolio=false, Weight=0});
-                }
-            
-            return result;  
-
+            tickers = new ArrayList(p.ComponentInfoList.Where(x => x.IsSelected).ToArray());
         }
 
     }
