8d85981b95af49befe54297c535c014aad1c505d
Romain Pouzaud <romain.pouzaud@orange.fr>
2014-09-15 18:43:37 +0200

    delete duplicate test

--- WallRiskEngineUnitTests/WallRiskEngineTest1.cs
+++ WallRiskEngineUnitTests/WallRiskEngineTest1.cs
@@ -37,21 +37,5 @@ namespace WallRiskEngineUnitTests
                 Assert.Fail();
             }
         }
-
-        [TestMethod]
-        public void ComputeCovarianceMatrixTest()
-        {
-            double[,] returns = { {0.05, 0.1, 0.6}, {0.001, 0.4, 0.56}, {0.7, 0.001, 0.12}, {0.3, 0.2, 0.1},
-                            {0.1, 0.2, 0.3}};
-
-            try
-            {
-                WallRiskEngine.API.computeCovarianceMatrix(returns);
-            }
-            catch (Exception e)
-            {
-                Assert.Fail();
-            }
-        }
     }
 }
